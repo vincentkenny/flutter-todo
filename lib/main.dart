e6442789dfb7feb020b3bb5d302cca9701015478
Kenny Vincent <vincent.kenny98@gmail.com>
2020-11-20 14:00:22 +0700

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_todo/services/auth.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to Flutter"),
-        ),
-        body: const Center(
-          child: Text("Hello World"),
-        ),
-      ),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Scaffold(body: Center(child: Text("Error")));
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Root();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Scaffold(body: Center(child: Text("Loading")));
+      },
+    );
+  }
+}
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth(auth: _auth).user,
     );
   }
 }
